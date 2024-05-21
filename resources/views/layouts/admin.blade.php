4b8611d8b3c4f118c98ae8d9f4405e1b6af5374c
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-21 16:36:07 +0200

    [Errore bootstrap]

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css, resources/js/app.js'])
     <title>Laravel Boolfolio</title>
+    @vite(['resources/css/app.css, resources/js/app.js'])
 </head>
 
 <body>
